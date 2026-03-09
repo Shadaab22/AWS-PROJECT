d87ef3ecfd22d1ae05c61c6a07331ff480f10b88
Shadaab22 <shadaab0510@gmail.com>
2026-03-09 20:22:54 +0530

    changed sns arn

--- stocker-main/app.py
+++ stocker-main/app.py
@@ -29,8 +29,8 @@ TRANSACTION_TABLE = "stocker_transactions"
 PORTFOLIO_TABLE = "stocker_portfolio"
 
 # SNS Topics
-USER_ACCOUNT_TOPIC_ARN = "arn:aws:sns:us-east-1:994357609447:StockerUserAccountTopic"
-TRANSACTION_TOPIC_ARN = "arn:aws:sns:us-east-1:994357609447:StockerTransactionTopic"
+USER_ACCOUNT_TOPIC_ARN = "arn:aws:sns:us-east-1:202533523387:StockerUserAccountTopic"
+TRANSACTION_TOPIC_ARN = "arn:aws:sns:us-east-1:202533523387:StockerTransactionTopic"
 
 
 # ================= HELPER CLASSES =================
