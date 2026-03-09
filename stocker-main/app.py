84154915757620772f55c8dbd98ee36c9560e574
Shadaab22 <shadaab0510@gmail.com>
2026-03-09 12:57:11 +0530

    Made changes

--- stocker-main/app.py
+++ stocker-main/app.py
@@ -411,5 +411,7 @@ def logout():
 # ================= RUN =================
 
 if __name__ == "__main__":
+    for rule in app.url_map.iter_rules():
+        print(rule.endpoint, rule)
 
     app.run(debug=True, host="0.0.0.0", port=5000)
